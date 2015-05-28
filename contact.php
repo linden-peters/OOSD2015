94a630a47f74e9f0a97340aec9d137a6618efbb1
linden-peters <linden-peters@users.noreply.github.com>
2015-05-28 08:34:21 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,13 +5,44 @@
  */
 require("header.php");
 include("db.php");
-
-          
+function formatPhone($phone)   //function to format the phone/fax number
+{
+    $phone = preg_replace("/[^0-9]/", "", $phone);
+    if(strlen($phone) == 7)  
+        return preg_replace("/([0-9]{3})([0-9]{4})/", "$1-$2", $phone);  //format 223-1258
+    elseif(strlen($phone) == 10)
+        return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "$1-$2-$3", $phone);  //format 403-223-2458
+    else
+        return $phone;
+}
 ?>
 <link href="contact.css" type="text/css" rel="stylesheet">   
 
 <script>
     var address = "";  
+	
+	function phoneValidate()  /* checking the entered value for the phone number. the format should be 403-256-1458 */
+	{
+	   	var msg;
+		var validno = new RegExp(/[0-9]{3}-[0-9]{3}-[0-9]{4}/);    
+		if (!validno.test(frmcontact.phoneno.value))
+		{
+			msg = "The phone number you entered is not valid!";
+			document.getElementById("error").innerHTML = msg;
+			document.getElementById("error").style.color="red";
+			return false;
+		}
+		else
+		{
+		  msg = "";
+		  document.getElementById("error").innerHTML = msg;
+		  document.getElementById("error").style.color="red";
+		  return true;
+		}
+	}
+	
+	
+	
 	function changeMousePointer(i)
 	{
 		document.getElementById("leftcol"+ i).style.cursor = "pointer";
@@ -23,7 +54,6 @@ include("db.php");
 		newwin = window.open("contactdetail.php?id='" + agencyid +"'",'','width=870 height=400 menubar=no');   
 		newwin.moveTo(300,250);
 	}
-
     function validate()   /* checking if the feilds are empty or not*/
 	{
 	     var message = "";
@@ -64,9 +94,12 @@ include("db.php");
 			 myform.comments.style.backgroundColor ="#F7BB86";
 		   }
 		   
+		   
+		   
 		   if (message == "")
 		   {
-		      return true;
+		      return phoneValidate();
+			  return true;
 		   }
 		   else
 		   {
@@ -74,7 +107,10 @@ include("db.php");
 				document.getElementById("error").innerHTML = message;
 				document.getElementById("error").style.color = "red";
 				return false;
-		   }  	
+		   }  
+           
+		    
+	
 	}
 </script>
 
@@ -98,13 +134,19 @@ include("db.php");
 				echo " <table>";
 					echo "<tr><td width=200 >";
 					$mainaddress = "";
+					$phoneno = "";
+					$faxno = "";
 					$mainaddress  = "Address:$row[AgncyAddress]<br />";
 					$mainaddress .= "$row[AgncyCity] \n";
 					$mainaddress .= "$row[AgncyProv] <br />";
 					$mainaddress .= "$row[AgncyPostal] \n";
 					$mainaddress .= "$row[AgncyCountry] <br />";
-					$mainaddress .= "Phone No: $row[AgncyPhone] <br />";
-					$mainaddress .= "Fax No: $row[AgncyFax] <br />";
+					
+					$phoneno = formatPhone("$row[AgncyPhone]");  //calling for formatphone function so that the phone number can be display in this format xxx-xxx-xxxx
+					$mainaddress .= "Phone No: $phoneno <br />";
+					
+					$faxno = formatPhone("$row[AgncyFax]");   //calling for formatphone function so that the fax number can be display in this format xxx-xxx-xxxx
+					$mainaddress .= "Fax No: $faxno <br />";
 					
 					echo "<p id='leftcol$i' onmouseover='changeMousePointer($i)' onClick='mouseClick($row[AgencyId])'>";
 					echo "$mainaddress";
@@ -146,7 +188,7 @@ include("db.php");
 				    </tr>
 				    <tr>
 				       <td class="tblleftcol"><label for="phoneno">Phone Number:</label></td> 
-					   <td align="left"><input type="text" tabindex=4 maxlength=12 size=30 placeholder="XXX-XXX-XXXX" name="phoneno"></td>   
+					   <td align="left"><input type="text" tabindex=4 maxlength=12 size=30 placeholder="XXX-XXX-XXXX" name="phoneno" onBlur="phoneValidate()"></td>   
 				    </tr>
 				    <tr>
 				       <td class="tblleftcol"><label for="comments">Comments:</label></td>
